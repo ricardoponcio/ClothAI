cff52ddfc0090ad16d157340e48c9469dd0f7b76
Ricardo Poncio <ricardo.poncio@outlook.com.br>
2024-11-28 13:12:18 -0300

    first commit

---
+++ src/main/java/dev/poncio/ClothAI/ClothAiTryOnApplication.java
@@ -0,0 +1,13 @@
+package dev.poncio.ClothAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothAiTryOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothAiTryOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/poncio/ClothAI/ClothAiTryOnApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.poncio.ClothAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothAiTryOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
